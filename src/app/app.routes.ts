24ad790bb196324ef18d90fbe3b50ace9c3e8db7
humildementebueno <humildementebueno@gmail.com>
2024-10-13 21:19:29 -0400

    feat: Actualizando versiones Angular cli 17.3.0 , node 18.20.3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HeroComponent } from './components/inicio/hero.component';
+import { HeroComponent } from './components/hero/hero.component';
 import { EventAnnouncementComponent } from './components/event-announcement/event-announcement.component';
 // import { HeroComponent } from './components/hero/hero.component';
 // import { EventAnnouncementComponent } from './components/event-annoukncement/event-announcement.component';
@@ -10,8 +10,8 @@ export const routes: Routes = [
   { path: '', component: NavbarComponent },
   { path: '', component: HeroComponent },
   { path: '', component: EventAnnouncementComponent },
-  { path: '', component: HeroComponent },
-  { path: '', component: HeroComponent },
+  { path: '', component: NavbarComponent },
+//   { path: '', component: HeroComponent },
 //   { path: 'event', component: EventAnnouncementComponent },
   // Otras rutas aquí
 ];
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importa CommonModule para el soporte de directivas básicas
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], // Asegúrate de importar módulos necesarios
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss'] // Cambiado a "styleUrls" (en plural)
 })
 export class NavbarComponent {
-
 }
